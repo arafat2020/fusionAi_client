e3df73b55c5ceacacd49996b9a8a0e96181bcccc
Arafat <arafatmannan2019@gmail.com>
2023-09-06 15:40:49 +0600

    loader fix

--- src/components/loder/Loader.jsx
+++ src/components/loder/Loader.jsx
@@ -20,6 +20,8 @@ function Loader({ limit = 6 }) {
                   boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
                   backdropFilter: "blur(5px)",
                   WebkitBackdropFilter: "blur(5px)",
+                  height:"90%",
+                  width:"35%"
                 }}
               />
               <div className="w-[60%]">
